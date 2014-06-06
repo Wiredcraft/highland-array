61435d7f0ce29a72dcb81ce6ccb8c7d6849afacc
Makara Wang <makara15@gmail.com>
2014-06-06 11:51:51 +0800

    Now use Object.defineProperty() to define the prototype.

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ var _;
 
 _ = require('highland');
 
-Array.prototype.shiftToStream = function(cb) {
-  if ((cb != null) && typeof cb !== 'function') {
-    throw new TypeError(cb + ' is not a function');
+Object.defineProperty(Array.prototype, 'shiftToStream', {
+  configurable: true,
+  enumerable: false,
+  writable: true,
+  value: function(cb) {
+    if ((cb != null) && typeof cb !== 'function') {
+      throw new TypeError(cb + ' is not a function');
+    }
+    return _((function(_this) {
+      return function(push, next) {
+        if (!_this.length) {
+          return push(null, _.nil);
+        }
+        push(null, cb ? cb(_this.shift()) : _this.shift());
+        return next();
+      };
+    })(this));
   }
-  return _((function(_this) {
-    return function(push, next) {
-      if (!_this.length) {
-        return push(null, _.nil);
-      }
-      push(null, cb ? cb(_this.shift()) : _this.shift());
-      return next();
-    };
-  })(this));
-};
+});
--- src/index.coffee
+++ src/index.coffee
@@ -1,9 +1,14 @@
 _ = require('highland')
 
-Array::shiftToStream = (cb) ->
-    throw new TypeError(cb + ' is not a function') if cb? and typeof cb isnt 'function'
-    return _((push, next) =>
-        return push(null, _.nil) if not @length
-        push(null, if cb then cb(@shift()) else @shift())
-        next()
-    )
+Object.defineProperty(Array.prototype, 'shiftToStream', {
+    configurable: true
+    enumerable: false
+    writable: true
+    value: (cb) ->
+        throw new TypeError(cb + ' is not a function') if cb? and typeof cb isnt 'function'
+        return _((push, next) =>
+            return push(null, _.nil) if not @length
+            push(null, if cb then cb(@shift()) else @shift())
+            next()
+        )
+})
